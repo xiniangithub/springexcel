c68900f61d3ec90caf36b68594a6fd88e19daddf
xiniangithub <2779207210@qq.com>
2017-09-26 16:11:23 +0800

    springexcel excel导出和下载

--- src/main/java/com/soft863/controller/IndexController.java
+++ src/main/java/com/soft863/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.soft863.controller;
 import com.soft863.entity.Result;
 import com.soft863.service.IndexService;
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
@@ -18,9 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.*;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 控制器
@@ -115,10 +116,65 @@ public class IndexController {
     }
 
     @RequestMapping("/exportExcel")
-    public void exportExcel() {
+    public ResponseEntity<byte[]> exportExcel() throws IOException {
+        List<Map<String, Object>> list = indexService.query();
 
+        // 创建
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("1");
+        for (int i=0; i<list.size(); i++) {
+            HSSFRow row = sheet.createRow(i);
 
+            Map<String, Object> map = list.get(i);
+            Set<String> keys = map.keySet();
+            Iterator<String> iterator = keys.iterator();
+            for(int j=0; iterator.hasNext(); j++) {
+                String key = iterator.next();
+                Object obj = map.get(key);
 
+                if(obj instanceof String) {
+                    String objStr = (String) obj;
+                    row.createCell(j).setCellValue(objStr);
+                } else if(obj instanceof Integer) {
+                    int objInt = (Integer) obj;
+                    row.createCell(j).setCellValue(objInt);
+                } else if(obj instanceof Long) {
+                    long objLong = (Long) obj;
+                    row.createCell(j).setCellValue(objLong);
+                } else if(obj instanceof Float) {
+                    float objFloat = (Float) obj;
+                    row.createCell(j).setCellValue(objFloat);
+                } else if(obj instanceof Double) {
+                    double objDouble = (Double) obj;
+                    row.createCell(j).setCellValue(objDouble);
+                } else if(obj instanceof Boolean) {
+                    Boolean objBoolean = (Boolean) obj;
+                    row.createCell(j).setCellValue(objBoolean);
+                } else if(obj instanceof Date) {
+                    Date objDate = (Date) obj;
+                    row.createCell(j).setCellValue(objDate);
+                }
+
+            }
+        }
+
+        String filename = "用户信息.xls";
+        // 获取下载文件
+        File file = new File(filename);
+        // 将excel文件内容写到excel文件中
+        workbook.write(file);
+
+        // 解决文件下载时中文文件名乱码
+        filename = new String(filename.getBytes("UTF-8"),"iso-8859-1");
+
+        // 设置头信息
+        HttpHeaders headers = new HttpHeaders();
+        // 设置浏览器以attachment（下载方式）
+        headers.setContentDispositionFormData("attachment", filename);
+        // application/octet-stream ： 二进制流数据
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
     }
 
 }
